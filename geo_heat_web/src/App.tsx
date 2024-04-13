64595f50a9dd7b261fdaa82eddb4a76774c8fb6d
Johannes <johannes.haick@gmail.com>
2024-04-13 15:23:58 +0200

    add map

--- geo_heat_web/src/App.tsx
+++ geo_heat_web/src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ComposableMap, Geographies, Geography } from "react-simple-maps"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const geoUrl =
+  "C:\Entwicklung\geo_heat\geo_heat_web\src\assets\pasted.json"
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ComposableMap>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography key={geo.rsmKey} geography={geo} />
+            ))
+          }
+        </Geographies>
+      </ComposableMap>
     </>
   )
 }
