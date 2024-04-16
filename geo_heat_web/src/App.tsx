a96b85086883d7efe15ec7c34f802e588c8408fc
Simon Krempin <67335158+simonkrempin@users.noreply.github.com>
2024-04-16 22:31:58 +0200

    added world map

--- geo_heat_web/src/App.tsx
+++ geo_heat_web/src/App.tsx
@@ -1,25 +1,28 @@
-import { ComposableMap, Geographies, Geography } from "react-simple-maps"
-import './App.css'
+import {
+    ComposableMap,
+    Geographies,
+    Geography,
+} from "react-simple-maps";
+import "./App.css";
 
 function App() {
 
-  const geoUrl =
-  "C:\Entwicklung\geo_heat\geo_heat_web\src\assets\pasted.json"
+    const geoUrl = "/geo-data.json";
 
-
-  return (
-    <>
-      <ComposableMap>
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => (
-              <Geography key={geo.rsmKey} geography={geo} />
-            ))
-          }
-        </Geographies>
-      </ComposableMap>
-    </>
-  )
+    return (
+        <ComposableMap>
+            <Geographies geography={geoUrl}>
+                {({ geographies }) =>
+                    geographies.map((geo) => (
+                        <Geography
+                            key={geo.rsmKey}
+                            geography={geo}
+                        />
+                    ))
+                }
+            </Geographies>
+        </ComposableMap>
+    );
 }
 
-export default App
+export default App;
