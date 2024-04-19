915375caf6a8961ed88f6dda55fa7570a9dc6f19
Simon Krempin <67335158+simonkrempin@users.noreply.github.com>
2024-04-19 13:36:01 +0200

    zoomable map

--- geo_heat_web/src/App.tsx
+++ geo_heat_web/src/App.tsx
@@ -2,25 +2,43 @@ import {
     ComposableMap,
     Geographies,
     Geography,
+    ZoomableGroup,
 } from "react-simple-maps";
+import React from "react";
 import "./App.css";
 
+interface Position {
+    coordinates: [number, number];
+    zoom: number;
+}
+
 function App() {
+    const [position, setPosition] = React.useState<Position>({ coordinates: [0, 0], zoom: 1 });
+
+    const handleMoveEnd = (position: Position) => {
+        setPosition(position)
+    }
 
     const geoUrl = "/geo-data.json";
 
     return (
         <ComposableMap>
-            <Geographies geography={geoUrl}>
-                {({ geographies }) =>
-                    geographies.map((geo) => (
-                        <Geography
-                            key={geo.rsmKey}
-                            geography={geo}
-                        />
-                    ))
-                }
-            </Geographies>
+            <ZoomableGroup
+                zoom={position.zoom}
+                center={position.coordinates}
+                onMoveEnd={handleMoveEnd}
+            >
+                <Geographies geography={geoUrl}>
+                    {({ geographies }) =>
+                        geographies.map((geo) => (
+                            <Geography
+                                key={geo.rsmKey}
+                                geography={geo}
+                            />
+                        ))
+                    }
+                </Geographies>
+            </ZoomableGroup>
         </ComposableMap>
     );
 }
